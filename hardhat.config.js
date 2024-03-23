b1e4df4719712392140001216ac0ce99d43d20d5
Varun Doshi <doshivarun202@gmail.com>
2024-03-23 11:48:48 +0530

    update:rpc addition

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,6 +5,12 @@ const { PRIVATE_KEY, USER_KEY } = process.env;
 module.exports = {
   solidity: "^0.8.0",
   networks: {
+    scrollTestnet: {
+      chainId: 534351,
+      url: "https://sepolia-rpc.scroll.io/",
+      accounts: process.env.PRIVATE_KEY,
+    },
+
     ten: {
       chainId: 443,
       url: `https://testnet.obscu.ro/v1/${USER_KEY}`,
